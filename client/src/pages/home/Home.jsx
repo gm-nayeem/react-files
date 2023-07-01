986f335f3cd8920ad669a8890b9df68baa01acad
golam mostafa <gmnayeem25@gmail.com>
2023-07-02 02:19:19 +0600

    design done

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import DropInputFile from '../../components/dropInputFile/DropInputFile';
 
 const Home = () => {
+  const handleFileChange = (files) => {
+    console.log(files);
+  }
+
   return (
-    <div>Home</div>
+    <div className="box">
+      <h2 className="header">
+        React Drag & Drop Files
+      </h2>
+      <DropInputFile
+        handleFileChange={handleFileChange}
+      />
+    </div>
   )
 }
 
-export default Home
+export default Home;
